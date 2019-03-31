4105651e372cc9d93ad374db77c88519d80aad57
Albert Richard Caputo III <rcaputo3@outlook.com>
2019-03-31 10:49:54 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # daily_coding_problem
+Problems from Daily Coding Problem book and subscription service. We will outline the problem description followed by an attempt and then the solution.
---
+++ arrays/prod_all_other_elts.py
@@ -0,0 +1,6 @@
+"""
+We want to obtain the product of all other elements in an array for each current element in the array.
+Can we do this without using the division operator?
+"""
+
+def arr_prod_w_div
