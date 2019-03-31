b54c047e41b8d7973a46780deae9822db0d94724
Albert Richard Caputo III <rcaputo3@outlook.com>
2019-03-31 15:21:28 -0400

    added second arr problem and edited first arr problem

---
+++ arrays/min_arr_range.py
@@ -0,0 +1,31 @@
+""" Return a tuple representing the minumum index range of an array that is out of order.
+    For example, the array [3, 7, 5, 6, 9] would return (1, 3). """
+
+def min_arr_range(arr):
+    """ Returns minimum index range that is out of order.
+    Input:
+        arr: numeric list
+    Returns:
+        (n, m) where n and m represent integer indeces.
+    """
+
+    max_seen, min_seen = -float("inf"), float("inf")
+    left, right = None, None
+
+    for i, v in enumerate(arr):
+        max_seen = max(max_seen, v)
+        if v < max_seen:
+            right = i
+
+    for i, v in enumerate(reversed(arr)):
+        min_seen = min(min_seen, v)
+        if v > min_seen:
+            # Correct reversed index
+            left = len(arr) - 1 - i
+
+    return (left, right)
+
+if __name__ == "__main__":
+    # Test cases
+    print(min_arr_range([3, 7, 5, 6, 9]))
+    print(min_arr_range([-1, 4, -30, 2, 1]))
--- arrays/prod_all_other_elts.py
+++ arrays/prod_all_other_elts.py
@@ -7,11 +7,22 @@ def arr_prod_w_div(arr):
     """ Takes an array as input and
     returns the product of all other elements for each element
     as an array. """
-    prod = 1
+
+    # Get total product and zero count
+    prod, zero_count = 1, 0
     for v in arr:
-        prod *= v
-    # Doesn't work if 0 is in the array.
-    return [prod//v if v != 0 else 0 for v in arr]
+        if not v:
+            # If more than 1 zero, return all zeros
+            if zero_count == 1:
+                return [0 for v in arr]
+            zero_count += 1
+        prod *= v if v else 1
+
+    # Correct for single 0
+    if zero_count:
+        return [0 if v else prod for v in arr]
+    return [prod//v for v in arr]
+
 
 
 def arr_prod(arr):
@@ -23,15 +34,19 @@ def arr_prod(arr):
     # left[i] = product of each elt to left of elt i
     for v in arr[:-1]:
         left.append(left[-1]*v)
+
     # right[i] = product of each elt to right of elt i
     for v in reversed(arr[1:]):
         right.append(right[-1]*v)
     right.reverse()
+
     # prod[i] = left[i]*right[i]
     return [l*r for l, r in zip(left, right)]
 
 
 if __name__ == "__main__":
     # Test cases
+    print(arr_prod_w_div([1, 2, 3, 4, 0, 0]))
+    print(arr_prod_w_div([4, 3, 2]))
     print(arr_prod([1, 2, 3, 4, 5]))
     print(arr_prod([4, 3, 2]))
